191090f5cb3673049e5affbd6dcc83dabe1b1834
Alex Poulsen <AlexPoulsen@users.noreply.github.com>
2019-01-15 16:05:12 -0500

    bad midi files now are skipped and can be retried

--- ninsheetmusic.py
+++ ninsheetmusic.py
@@ -41,15 +41,36 @@ except OSError as e:
 	if e.errno != errno.EEXIST:
 		raise
 counter = 1
+failed = []
 print("downloading files")
 print("     ", end="")
 for file in dl:
 	new_filename = get_filename_from_cd(urllib.request.urlopen(file).headers.get('content-disposition'), counter)
-	if new_filename is None:
-		new_filename = "unnamed_" + str(counter) + ".mid"
-	urllib.request.urlretrieve(file, str(path / new_filename))
+	if new_filename is not None:
+		urllib.request.urlretrieve(file, str(path / new_filename))
+	else:
+		failed.append([file, str(path / "unnamed_" + str(counter) + ".mid"]))
 	print("|", end="")
 	if counter % 120 == 0:
 		print("\n", end="     ")
 	counter += 1
 print("\n")
+failed_answer = input("Retry files with a failed name retrieve? [y/N] ")
+if failed_answer.lower() == "y":
+	for file in failed:
+		urllib.request.urlretrieve(*file)
+		print("|", end="")
+		if counter % 120 == 0:
+			print("\n", end="     ")
+		counter += 1
+	print("\n")
+elif failed_answer.lower() == "n":
+	pass
+else:
+	for file in failed:
+		urllib.request.urlretrieve(*file)
+		print("|", end="")
+		if counter % 120 == 0:
+			print("\n", end="     ")
+		counter += 1
+	print("\n")
